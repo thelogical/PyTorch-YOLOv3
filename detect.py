db50ab752337f9370ee6784442fb5f99dd935800
Swapnil <coolboy20x@gmail.com>
2019-12-26 18:10:38 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -114,8 +114,7 @@ if __name__ == "__main__":
             for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
 
                 print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
-                line = "x1,"+"y1,"+"x2,"+"y2,"+"cls_conf.item(),"+"classes[int(cls_pred)],'\n'"
-                f.write(line)
+                print(x1,y1,x2,y2,cls_conf.item(),classes[int(cls_pred)],file=f)
                 box_w = x2 - x1
                 box_h = y2 - y1
 
